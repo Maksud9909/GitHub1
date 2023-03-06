627e43dcf6de24875f3217b8e24d0c389ddbb39d
maksud9909 <rmaksud822@gmail.com>
2023-03-06 21:06:22 +0500

    Update Test2.java

--- src/Test2/Test2.java
+++ src/Test2/Test2.java
@@ -2,6 +2,11 @@ package Test2;
 
 public class Test2 {
     public static void main(String[] args) {
-        
+
     }
+    public int close10(int a, int b) {
+        int result = 10-a;
+        if (result)
+    }
+
 }
